7398c667882b409873f197227587043a0265361f
Stefan Cucuta <cucutastefan@gmail.com>
2022-02-03 11:28:02 +0000

    Add game board and ensure that the ships fits to it

--- run.py
+++ run.py
@@ -21,14 +21,88 @@ GUESSED = 'O'
 # Used to separate the different phases of the game
 PHASE = '░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
 
+# Welcome message used at the begaining
 
-def run_game():
+def welcome_message():
     
-    print('WELCOME TO BATTLESHIPS!')
+    print('WELCOME TO BATTLESHIPS GAME!')
     print('THE BOARD IS A GRID OF 10X10 WITH FOUR SHIPS TO SINK')
     print('AIRCRAFT CARRIER - BATTLECRUISER - SUBMARINE - FRIGATE')
-    print('A TOTAL SCORE OF 15 IS REQUIRED TO WIN, 1 POINT PER HIT\n')
-    print('NAME CAN BE 10 CHARACTERS MAX. LETTERS, NUMBERS & UNDERSCORES ONLY')
+    print('EACH PLAYER HAS 15 LIVES, THEY LOSE 1 FOR EACH HIT FROM THE OPPONENT\n')
+    print(f"{EMPTY} IS FOR AN EMPTY OR CO-ORDINATE THAT HASN'T BEEN GUESSED")
+    print(f'{SHIP} REPRESENTS A SHIP')
+    print(f'{HITSHIP} REPRESENTS A HIT OR SUNK SHIP')
+    print(f'{GUESSED} IS FOR A CO-ORDINATE THAT HAS BEEN GUESSED\n')
 
+# Validate the name of the team
 
+def validate_team_name(name):
+    
+    if not re.match('^[A-Za-z0-9_]*$', name):
+        print('INVALID NAME. LETTERS, NUMBERS AND UNDERSCORES ONLY')
+        return False
+    elif len(name) > 10:
+        print('INVALID NAME. 10 CHARACTERS MAX')
+        return False
+    elif len(name) == 0:
+        print('INVALID NAME. NAME NOT LONG ENOUGH')
+    else:
+        return True
+
+# Game Board
+
+class GameBoard:
+
+    # Converts letters for display purposes to numbers for functionality
+    letters_to_numbers = {
+        'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4,
+        'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9
+        }
+
+    # Valid row input
+    row_input = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+
+    def __init__(self, name, user):
+        self.board = [[EMPTY] * 10 for x in range(10)]
+        self.lives = 15
+        self.name = name
+        self.user = user
+        self.column_arry = [10]
+        self.row_arry = [10]
+        self.attk_arry = [1, 1, 1, 1]
+
+    # Prints the game board
+
+    def print_board(self):
+        
+        print(f'{self.name} BOARD:\n')
+        print('  A B C D E F G H I J ')
+        print('  -------------------')
+        row_number = 0
+        for row in self.board:
+            print('%d|%s ' % (row_number, ' '.join(row)))
+            row_number += 1
+        print(f'\nLIVES REMAINING: {self.lives}\n')
+
+    # Checks if the ships fits to the board
+
+    def check_ship_fits(self, ship_length, row, column, orientation):
+        if orientation == 'H':
+            if column + ship_length > 10:
+                if self.user == 'player':
+                    print('SHIP DOES NOT FIT, TRY AGAIN!\n')
+                    return False
+                else:
+                    return False
+            else:
+                return True
+        else:
+            if row + ship_length > 10:
+                if self.user == 'player':
+                    print('SHIP DOES NOT FIT, TRY AGAIN!\n')
+                    return False
+                else:
+                    return False
+            else:
+                return True
 run_game()
